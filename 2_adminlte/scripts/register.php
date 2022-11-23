af80e8bb4457b51669a7c2854c3e8ef37445a546
filipwroblewski <filip.wroblewski.row@gmail.com>
2022-11-23 17:36:43 +0100

    .

---
+++ 2_adminlte/scripts/register.php
@@ -0,0 +1,27 @@
+<?php
+    $error = 0;
+    foreach ($_POST as $key => $value){
+        if (empty($value)){
+            $error = 1;
+        }
+    }
+
+    if (!isset($_POST['agreeTerms'])){
+        $error = 1;
+    }
+
+    if ($error == 1){
+        echo "<script>history.back()</script>";
+        exit();
+    }
+
+    require_once 'connect.php';
+    // $sql = "INSERT INTO `users` (`id`, `city_id`, `name`, `surname`, `birthday`, `created_at`, `email`, `pass`) VALUES (NULL, \'1\', \'asd\', \'asd\', \'2022-11-03\', current_timestamp(), \'asd@wp.pl\', \'pswd\');";
+    // $mysqli->query($sql);
+
+    
+    //https://www.php.net/manual/en/mysqli.quickstart.prepared-statements.php
+    $stmt = $mysqli->prepare("INSERT INTO users(city_id, name, surname, email, pass, birthday) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("isssss", $_POST['city_id'], $_POST['name'], $_POST['surname'], $_POST['email1'], $_POST['pass1'], $_POST['birthday']); // isssss -> inteager, string, string, string, string, string, string
+    $stmt->execute();
+?>
--- 2_adminlte/view/register.php
+++ 2_adminlte/view/register.php
@@ -16,47 +16,88 @@
       <a href="../" class="h1"><b>Admin</b>LTE</a>
     </div>
     <div class="card-body">
-      <p class="login-box-msg">Register a new membership</p>
+      <p class="login-box-msg">Rejestracja użytkownika</p>
 
-      <form action="../../index.html" method="post">
+      <form action="../scripts/register.php" method="post">
+
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
+          <input type="text" class="form-control" placeholder="Podaj imię" name="name">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
             </div>
           </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" class="form-control" placeholder="Podaj naziwsko" name="surname">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Podaj email" name="email1">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
             </div>
           </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="email" class="form-control" placeholder="Powtórz email" name="email2">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Podaj hasło" name="pass1">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="password" class="form-control" placeholder="Powtórz hasło" name="pass2">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
+
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Miasto" name="city_id">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-city"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="date" class="form-control" placeholder="Podaj date" name="birthday">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-calendar"></span>
+            </div>
+          </div>
+        </div>
+
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
-              <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+              <input type="checkbox" id="agreeTerms" name="agreeTerms" value="terms">
               <label for="agreeTerms">
-               I agree to the <a href="#">terms</a>
+               Zatwierdzam <a href="#">regulamin</a>
               </label>
             </div>
           </div>
