f06e75b3310124a25d0e54bc1e947d1e1c8f5dbd
filipwroblewski <filip.wroblewski.row@gmail.com>
2022-12-07 08:26:09 +0100

    To fix user register, but added passwor hash

--- 2_adminlte/scripts/register.php
+++ 2_adminlte/scripts/register.php
@@ -20,12 +20,13 @@
     require_once 'connect.php';
     // $sql = "INSERT INTO `users` (`id`, `city_id`, `name`, `surname`, `birthday`, `created_at`, `email`, `pass`) VALUES (NULL, \'1\', \'asd\', \'asd\', \'2022-11-03\', current_timestamp(), \'asd@wp.pl\', \'pswd\');";
     // $mysqli->query($sql);
+    $pass = password_hash($_POST['pass1'], PASSWORD_ARGON2ID);
 
     
     //https://www.php.net/manual/en/mysqli.quickstart.prepared-statements.php
     try{
         $stmt = $mysqli->prepare("INSERT INTO users(city_id, name, surname, email, pass, birthday) VALUES (?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("isssss", $_POST['city_id'], $_POST['name'], $_POST['surname'], $_POST['email1'], $_POST['pass1'], $_POST['birthday']); // isssss -> inteager, string, string, string, string, string, string
+        $stmt->bind_param("isssss", $_POST['city_id'], $_POST['name'], $_POST['surname'], $_POST['email1'], $pass, $_POST['birthday']); // isssss -> inteager, string, string, string, string, string, string
         $stmt->execute();
 
         if ($stmt->affected_rows == 1){
